cf774bd06360278897549768dacf7b31937ed00f
Felix Faerber <ffaerber@gmail.com>
2017-06-19 11:17:08 +0200

    multi tenant functionality

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,11 +1,29 @@
+require 'yaml'
 current_dir = File.dirname(__FILE__)
+organizations = YAML.load_file("#{current_dir}/organizations/index.yml")
+
+if ENV['ORGNAME'] # export ORGNAME='myorg' or unset ORGNAME
+  organization_name  = ENV['ORGNAME']
+  organization       = organizations[organization_name]
+  if organization
+    puts "knife is running with organization: #{organization_name}"
+  else
+    puts "Organization is '#{organization_name}' but it is not found in /organizations/index.yml"
+    exit!
+  end
+else
+  puts "You must specify organization. Possible organizations: #{organizations.keys}"
+  puts "export ORGNAME=name_of_the_organization"
+  exit!
+end
+
 log_level                :info
 log_location             STDOUT
-node_name                "ffaerber"
-client_key               "#{current_dir}/ffaerber.pem"
-validation_client_name   "myorg-validator"
-validation_key           "#{current_dir}/myorg-validator.pem"
-chef_server_url          "https://ec2-52-16-90-217.eu-west-1.compute.amazonaws.com/organizations/myorg"
+node_name                organization['node_name']
+client_key               "#{current_dir}/organizations/#{organization_name}/#{organization['client_key']}"
+validation_client_name   "#{organization_name}-validator"
+validation_key           "#{current_dir}/organizations/#{organization_name}/validator.pem"
+chef_server_url          organization['pub']
 syntax_check_cache_path  "#{ENV['HOME']}/.chef/syntaxcache"
 cookbook_path            ["#{current_dir}/../cookbooks"]
 
@@ -15,11 +33,11 @@ cookbook_path            ["#{current_dir}/../cookbooks"]
 # ami-ac524fca custom centos7 without SELinux
 # chmod 600 .chef/.aws/ffaerber.pem
 # ssh-add .chef/.aws/ffaerber.pem
-knife[:ssh_key_name]          = 'chef_cumulocity'
-knife[:aws_access_key_id]     = "AKIAJFCZY74TCITUMZYQ"
-knife[:aws_secret_access_key] = "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"
+knife[:ssh_key_name]          = organization['knife']['ssh_key_name']
+knife[:aws_access_key_id]     = organization['knife']['aws_access_key_id']
+knife[:aws_secret_access_key] = organization['knife']['aws_secret_access_key']
 knife[:availability_zone]     = 'eu-west-1a'
 knife[:region]                = 'eu-west-1'
 knife[:ssh_user]              = 'centos'
-knife[:image]                 = 'ami-ac524fca' # https://wiki.centos.org/Cloud/AWS
+knife[:image]                 = 'ami-ac524fca'
 knife[:flavor]                = 'm4.large'
