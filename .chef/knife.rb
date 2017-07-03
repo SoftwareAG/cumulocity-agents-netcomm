55d0d6a890611a8bfba5c9766d62c5852687dcaf
Felix Faerber <ffaerber@gmail.com>
2017-07-03 10:51:01 +0200

    better knife outputs

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -2,7 +2,7 @@ require 'yaml'
 current_dir = File.dirname(__FILE__)
 organizations = YAML.load_file("#{current_dir}/organizations/index.yml")
 
-if ENV['ORGNAME'] # export ORGNAME='myorg' or unset ORGNAME
+if ENV['ORGNAME']
   organization_name  = ENV['ORGNAME']
   organization       = organizations[organization_name]
   if organization
@@ -12,7 +12,6 @@ if ENV['ORGNAME'] # export ORGNAME='myorg' or unset ORGNAME
     exit!
   end
 else
-  puts
   puts "You must specify a organization. Possible organizations: #{organizations.keys.join(', ')}"
   puts "set the organization via: export ORGNAME=name_of_the_organization"
   exit!
@@ -29,16 +28,13 @@ syntax_check_cache_path  "#{ENV['HOME']}/.chef/syntaxcache"
 cookbook_path            ["#{current_dir}/../cookbooks"]
 
 
-# AWS
-# ami-0d063c6b default centos7
-# ami-ac524fca custom centos7 without SELinux
-# chmod 600 .chef/.aws/ffaerber.pem
-# ssh-add .chef/.aws/ffaerber.pem
 knife[:ssh_key_name]          = organization['knife']['ssh_key_name']
 knife[:aws_access_key_id]     = organization['knife']['aws_access_key_id']
 knife[:aws_secret_access_key] = organization['knife']['aws_secret_access_key']
+knife[:flavor]                = 'm4.large'
 knife[:availability_zone]     = 'eu-west-1a'
 knife[:region]                = 'eu-west-1'
 knife[:ssh_user]              = 'centos'
+# ami-0d063c6b default centos7
+# ami-ac524fca custom centos7 without SELinux
 knife[:image]                 = 'ami-ac524fca'
-knife[:flavor]                = 'm4.large'
