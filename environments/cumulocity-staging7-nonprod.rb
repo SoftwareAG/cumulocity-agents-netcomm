bb7c914c9b6d1369390de0d6d15ab08a14690b70
Jagat Jyoti Mishra (JMIS) <JagatJyoti.Mishra@softwareag.com>
2018-05-30 08:12:37 -0700

    Staging7 upgrade to 9.8

--- environments/cumulocity-staging7-nonprod.rb
+++ environments/cumulocity-staging7-nonprod.rb
@@ -40,8 +40,8 @@ override_attributes(
   },
   "cumulocity-karaf" => {
     "CUMULOCITY_LICENCE_KEY" => "17adb8fe8848af81a75d175bace5d013bf71ee4fa374aafb30313f3d245de270b5f953ab29861044ef6e169406fb469fc50407d31c81ba874e1a3b9b37a33bfc",
-    "version" => "9.7.0-1",
-    "ssa-version" => "9.7.0-1",
+    "version" => "9.8.0-1",
+    "ssa-version" => "9.8.0-1",
     "memory_left_for_system" => "2048",
     "notification" => true,
     "cep-server-enabled" => true,
@@ -67,7 +67,7 @@ override_attributes(
 
   "cumulocity-GUI" => {
     "connString" => "https://C8YWebApps:dkieW^s99l0@resources.cumulocity.com/targets/cumulocity/e153c733d590",
-    "version" => '9.7.0'
+    "version" => '9.8.0'
   },
   "cumulocity-ssagents" => {
     "useTags" => true,
