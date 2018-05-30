5a0c671b95277c6d50e50d4994dbf95d43912e5d
Jacek Rzeznik <jacek.rzeznik.ext@nsn.com>
2018-05-30 12:20:08 +0200

    Staging7: changing SSAgent version

--- environments/cumulocity-staging7-nonprod.rb
+++ environments/cumulocity-staging7-nonprod.rb
@@ -41,7 +41,7 @@ override_attributes(
   "cumulocity-karaf" => {
     "CUMULOCITY_LICENCE_KEY" => "17adb8fe8848af81a75d175bace5d013bf71ee4fa374aafb30313f3d245de270b5f953ab29861044ef6e169406fb469fc50407d31c81ba874e1a3b9b37a33bfc",
     "version" => "9.7.0-1",
-    "ssa-version" => "9.6.1-1",
+    "ssa-version" => "9.7.0-1",
     "memory_left_for_system" => "2048",
     "notification" => true,
     "cep-server-enabled" => true,
