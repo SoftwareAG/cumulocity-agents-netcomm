b7ad95ac54811c0c0da7c2a1f71439334c66f5e4
Bartosz Bubak <bartosz.bubak@gmail.com>
2018-09-25 20:48:04 +0200

    MTM-19175 update scripts

--- auto_provisioning.sh
+++ auto_provisioning.sh
@@ -10,11 +10,16 @@ cd ./environments/ && ./set_versions.sh jenkins.json
 knife environment from file auto_provisioning_env.json
 
 cd ..
+echo "Remove old vault"
 bundle exec knife vault delete secrets cumulocity-staging-jenkins-nonprod.core -M client -y
 bundle exec knife vault delete secrets cumulocity-staging-jenkins-nonprod.docker -M client -y
 
+echo "First run"
 bundle exec chef-client -l info -z provisioning/cumulocity-staging-jenkins-frankfurt-nonprod.rb
+
+echo "Add new vaults"
 bundle exec knife vault create secrets cumulocity-staging-jenkins-nonprod.docker -A 'admin12' -M client -S 'name:*' -J .chef/secrets/cumulocity-staging-jenkins-frankfurt-nonprod.docker.json
 bundle exec knife vault create secrets cumulocity-staging-jenkins-nonprod.core -A 'admin12' -M client -S 'name:*' -J .chef/secrets/cumulocity-staging-jenkins-frankfurt-nonprod.core.json
 
+echo "Second run"
 bundle exec chef-client -l info -z provisioning/cumulocity-staging-jenkins-frankfurt-nonprod.rb
