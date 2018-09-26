718d3cd78a3fe0d164fd1e3254721ce426da4eb4
Bartosz Bubak <bartosz.bubak@gmail.com>
2018-09-26 14:14:03 +0200

    MTM-19175 add IP script

--- get_lb_ip.sh
+++ get_lb_ip.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-knife search "node_name:cumulocity-staging-jenkins-nonprod_lb1" -F json | jq '.rows |= sort_by(.name) | .rows[] | .automatic.cloud_v2.public_ipv4'
+knife search "node_name:cumulocity-staging-jenkins-nonprod_lb1" -F json | jq '.rows |= sort_by(.name) | .rows[] | .automatic.cloud_v2.public_ipv4' | tr -d \" 
