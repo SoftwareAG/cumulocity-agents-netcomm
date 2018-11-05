13c276f7da125f77e383f503b72aa937f1d3fb72
Łukasz Pospiech <zocimek@gmail.com>
2018-11-05 11:53:57 +0100

    fix typo

--- staging-deployment.sh
+++ staging-deployment.sh
@@ -36,6 +36,7 @@ case $environment in
     staging-performance)
         export CHEF_ENV=cumulocity-staging-performance-test-nonprod
         export ORGNAME=cumulocity-devel
+    ;;
     *)
         echo "You need to choose a valid environment (one of: staging staging-1 staging-2 staging-007 staging-7 staging-latest)"
         exit 1
