cce9f4b3b5eacb773fe7680bd4cb5d278208fb43
Bartosz Bubak <bartosz.bubak@gmail.com>
2018-11-07 16:26:47 +0100

    Minor updates

--- auto_provisioning.sh
+++ auto_provisioning.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-export CUMULOCITY_KUBERNETES_IMAGE="9.12.8"
-export CUMULOCITY_KARAF_IMAGE="9.12.8-1"
-export CUMULOCITY_KARAF_SSA="9.12.8-1"
-export CUMULOCITY_GUI="9.12.8"
-export MODULES_LIST=cep,cep-small,device-simulator,smartrule,jwireless,apama-small,sms-gateway,billwerk-agent-server,connectivity-agent-server,branding,cellid-agent-server,remote-access,speech-agent-server
+export CUMULOCITY_KUBERNETES_IMAGE="9.16.2"
+export CUMULOCITY_KARAF_IMAGE="9.16.2-1"
+export CUMULOCITY_KARAF_SSA="9.16.2-1"
+export CUMULOCITY_GUI="9.16.2"
+export MODULES_LIST=cep,cep-small,device-simulator,smartrule,jwireless,apama-small,billwerk-agent-server,connectivity-agent-server,branding,cellid-agent-server,remote-access,speech-agent-server
 
 cd ./environments/ && ./set_versions.sh jenkins.json
 
@@ -16,11 +16,11 @@ bundle exec knife vault delete secrets cumulocity-staging-jenkins-nonprod.core -
 bundle exec knife vault delete secrets cumulocity-staging-jenkins-nonprod.docker -M client -y
 
 echo "First run"
-bundle exec chef-client -l info -z provisioning/cumulocity-staging-jenkins-frankfurt-nonprod.rb
+bundle exec chef-client -l info -z provisioning/cumulocity-staging-jenkins-frankfurt-nonprod_2.rb
 
 echo "Add new vaults"
 bundle exec knife vault create secrets cumulocity-staging-jenkins-nonprod.docker -A 'admin12' -M client -S 'name:*' -J .chef/secrets/cumulocity-staging-jenkins-frankfurt-nonprod.docker.json
 bundle exec knife vault create secrets cumulocity-staging-jenkins-nonprod.core -A 'admin12' -M client -S 'name:*' -J .chef/secrets/cumulocity-staging-jenkins-frankfurt-nonprod.core.json
 
 echo "Second run"
-bundle exec chef-client -l info -z provisioning/cumulocity-staging-jenkins-frankfurt-nonprod.rb
+bundle exec chef-client -l info -z provisioning/cumulocity-staging-jenkins-frankfurt-nonprod_2.rb
