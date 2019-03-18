b58d56149fcc6d44b7988b86ee2b980164390047
Bartosz Bubak <bartosz.bubak@gmail.com>
2019-03-18 12:39:32 +0100

    New version of dev-d provisioning

--- provisioning/cumulocity-development-dev-d.rb
+++ provisioning/cumulocity-development-dev-d.rb
@@ -18,22 +18,38 @@ with_machine_options({
   ssh_username: "centos"
 })
 
-add_machine_options(
+add_machine_options({
   bootstrap_options: {
     key_name: 'chef_cumulocity',
     instance_type: 'm3.medium',
     image_id: 'ami-0597ae12f89cbc55c',
     subnet_id: 'subnet-c477d0bf',
     security_group_ids: ['sg-02ed752df3d92fa8f']
-  }
-)
+  },
+  create_timeout: 360,
+  start_timeout: 360,
+  ssh_timeout: 360
+})
 
 private_ips = "172.31.18.177"
 flavour_for_dev = "m3.large"
 dev_id = "dev-d"
 
+step = ENV['STEP'].to_i || 1
 ### END OF CLUSTER CONFIGURATION ###
-machine "#{environment}_#{dev_id}" do
+
+ruby_block 'next-step-is' do
+  block do
+  puts ""
+  puts "===================================="
+  puts "T h e  c u r r e n t  s t e p  is  #{step}/4"
+  puts "===================================="
+  puts ""
+  sleep 4
+  end
+end
+
+machine "#{dev_id}" do
     add_machine_options(
         bootstrap_options: {
             # private_ip_address: "#{private_ips}",
@@ -43,9 +59,20 @@ machine "#{environment}_#{dev_id}" do
     tags ["standalone:mongod7:"]
     attributes(
       ddclient: {
-        domain: "#{dev_id}.cumulocity.com"
+        domain: "#{dev_id}.cumulocity.com",
+        login:  "13107q-m2mdyndns",
+        password: "p2AN8xG9)e.K",
+        use: "web, web=checkip.dyndns.com/, web-skip='IP Address'",
+        server: "members.dyndns.org"
       }
     )
-    recipe 'cumulocity-ddclient'
-    role 'cumulocity-dev-singlenode' 
+    if step > 1
+      recipe 'cumulocity-ddclient'
+      role 'cumulocity-base'
+      recipe 'cumulocity::mongo'
+      role 'cumulocity-common-cores'
+      role 'cumulocity-kubernetes' if step >= 3
+      recipe 'cumulocity::karaf_dev-x-agents' if step >= 3
+      role 'cumulocity-mn-active-core' if step >= 4
+    end
 end
