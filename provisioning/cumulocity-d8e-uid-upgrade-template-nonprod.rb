1758fa36a3aa0b87824206804a02de7193d9d3eb
Manfred Michels <Manfred.Michels@cumulocity.com>
2019-11-05 14:01:53 +0100

    MTM-26749 provisioning script for K8S masters and workers, can be used for migration from 1.8 to 1.16

---
+++ provisioning/cumulocity-d8e-uid-upgrade-template-nonprod.rb
@@ -0,0 +1,138 @@
+require 'chef/provisioning/aws_driver'
+with_driver 'aws:cumulocity:eu-central-1'
+
+environment  = '<environment name>-nonprod'
+
+with_chef_environment environment
+with_chef_server(
+  "https://chef12.cumulocity.com/organizations/cumulocity-devel",
+  client_name: Chef::Config[:node_name],
+  signing_key_filename: Chef::Config[:client_key]
+)
+
+with_machine_options({
+  convergence_options: {
+    ssl_verify_mode: 'verify_none',
+    chef_version: "12.21.31"
+  },
+  ssh_username: "centos"
+})
+
+add_machine_options(
+    bootstrap_options: {
+      key_name: 'chef_cumulocity',
+      instance_type: 'm3.medium',
+      # image_id: 'ami-60206719', #Ireland
+    #   image_id: 'ami-0597ae12f89cbc55c', #Frankfurt
+      image_id: 'ami-337be65c', #Frankfurt
+    #   image_id: 'ami-04cf43aca3e6f3de3',
+      subnet_id: 'subnet-c477d0bf',
+      security_group_ids: ['sg-02ed752df3d92fa8f']
+    }
+  )
+
+### CONFIGURE YOUR CLUSTER BELOW ###
+
+kubernetes_master_count   = 3
+flavour_for_masters       = "m4.xlarge"
+volume_size_for_masters   = 16
+
+kubernetes_worker_count   = 2
+flavour_for_workers       = "c4.2xlarge"
+volume_size_for_workers   = 30
+
+
+### END OF CLUSTER CONFIGURATION ###
+
+defaultStep = ENV['STEP'].to_i || 1
+
+initStep = Integer(::File.read("/tmp/.ps-#{environment}.steps").chomp) rescue defaultStep
+
+for step in initStep..7
+  if step >= 1
+
+  current_step = lambda {step}
+  file "/tmp/.ps-#{environment}.steps" do
+        content  "#{current_step.call}"
+  end
+
+  ruby_block 'next-step-is' do
+        block do
+        current_step = ::File.read("/tmp/.ps-#{environment}.steps").chomp
+        puts ""
+        puts "===================================="
+        puts "T h e  c u r r e n t  s t e p  is  #{current_step}"
+        puts "===================================="
+        puts ""
+        sleep 4
+        only_if { ::File.exist?("/tmp/.ps-#{environment}.steps") }
+        end
+  end
+
+
+  machine_batch do
+
+    1.upto(kubernetes_master_count) do |i|
+        machine "#{environment}_upgrade_master_#{i}" do
+        add_machine_options(
+            bootstrap_options: {
+                instance_type: "#{flavour_for_masters}",
+                block_device_mappings: [{
+                    'device_name': '/dev/sda1',
+                    'ebs': {
+                      'volume_size': "#{volume_size_for_masters}",
+                      'delete_on_termination': true }
+                }]
+
+            }
+        )
+        role 'cumulocity-base'
+        if step > 1
+            role 'cumulocity-kubernetes'
+            node_tags = []
+            node_tags << 'etcd'
+            node_tags << 'etcd-init' if step == 3
+            node_tags << 'k8s-master'
+            node_tags << 'k8s-master-main' if i == 1
+            node_tags << 'k8s-master-init' if step == 4 && i == 1
+            node_tags << 'k8s-master-add' if step >= 6 && i != 1
+            tags node_tags
+            recipe 'cumulocity-kubernetes::certs_upload' if step == 5 && i == 1
+        end
+        end
+    end
+
+    1.upto(kubernetes_worker_count) do |i|
+        machine "#{environment}_upgrade_worker_#{i}" do
+        add_machine_options(
+            bootstrap_options: {
+                instance_type: "#{flavour_for_workers}",
+                block_device_mappings: [{
+                    'device_name': '/dev/sda1',
+                    'ebs': {
+                      'volume_size': "#{volume_size_for_workers}",
+                      'delete_on_termination': true }
+                }]
+
+            }
+        )
+        role 'cumulocity-base'
+        if step > 1
+            role 'cumulocity-kubernetes'
+            node_tags = []
+            node_tags << 'k8s-worker' if step >= 5
+            tags node_tags
+            # role 'cumulocity-chaos-monkey' if step == 7
+        end
+        end
+    end
+
+  end      #######  machine_batch do
+
+ end       ###### if
+
+end        ###### for
+
+file "/tmp/.ps-#{environment}.steps" do
+    action :delete
+end
