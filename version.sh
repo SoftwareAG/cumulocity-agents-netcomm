495db84758d7c3535b6477a326455619d216be40
Rina Fujino <rina.fujino@cumulocity.com>
2020-02-07 17:42:03 +0100

    [fix]NTC-220: fix version.sh for cumulocity-ntc-agent

--- version.sh
+++ version.sh
@@ -1,21 +1,18 @@
 #!/usr/bin/env sh
-USAGE="Description: ntcagent and smsagent release number versioner.
+USAGE="Description: cumulocity-ntc-agent release number versioner.
 
 Usage:
-$(basename $0) -h                    -- Show this help.
-$(basename $0) ask                   -- Show ntcagent and smsagent current versions
-$(basename $0) <ntcagent> <smsagent> -- Update <ntcagent> and <smsagent> version number.
-                                      - for no update."
+$(basename $0) -h                            -- Show this help.
+$(basename $0) ask                           -- Show cumulocity-ntc-agent current version.
+$(basename $0) update <cumulocity-ntc-agent> -- Update <cumulocity-ntc-agent> version number.
+                                         -- for no update."
 
 if [ "$1" = "" -o "$1" = "-h" ]; then
     echo "$USAGE"
 elif [ "$1" = "ask" ]; then
-    echo -n "ntcagent: "
-    grep Version: debian/ntcagent/control | cut -c 10-
-    echo -n "smsagent: "
-    grep Version: debian/smsagent/control | cut -c 10-
-else
-    test $1 != '-' && sed -i "/Version:/c\Version: $1" debian/ntcagent/control
-    test $2 != '-' && sed -i "/Version:/c\Version: $2" debian/smsagent/control
+    echo -n "cumulocity-ntc-agent: "
+    grep Version: debian/c8yntcagent/control | cut -c 10-
+elif [ "$1" = "update" ]; then
+    test $2 != '-' && sed -i "/Version:/c\Version: $2" debian/c8yntcagent/control
 fi
 exit 0
